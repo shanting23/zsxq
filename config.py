31d1e89cc6ad580c7cb38aad2be3eb165d1618dc
shanting23 <115713438+shanting23@users.noreply.github.com>
2026-06-25 14:52:21 +1000

    Add CHANNELS configuration with channel details

---
+++ config.py
@@ -0,0 +1,22 @@
+CHANNELS = [
+    {
+        "name": "Meet Kevin",
+        "channel_id": "UCxxxxxxxxxxxxx"
+    },
+    {
+        "name": "Real Vision",
+        "channel_id": "UCyyyyyyyyyyyy"
+    },
+    {
+        "name": "Channel 3",
+        "channel_id": "UCzzzzzzzzzz"
+    },
+    {
+        "name": "Channel 4",
+        "channel_id": "UCaaaaaaaaaaa"
+    },
+    {
+        "name": "Channel 5",
+        "channel_id": "UCbbbbbbbbbbb"
+    }
+]
